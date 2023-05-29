3cc9d9d1d4fddbd21a176f9c25d0e27629024606
gumingcn <gumingcn@gmail.com>
2023-05-29 16:54:00 +0800

    first commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { Application , Request, Response} from 'express';
+import * as dotenv from "dotenv"
+
+
+dotenv.config();
+if (!process.env.PORT) {
+  process.exit(1);
+}
+const app: Application = express()
+
+const port: number = parseInt(process.env.PORT as string, 10);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello world!')
+})
+
+app.listen(port, function () {
+  console.log(`App is listening on port ${port} !`)
+})
