056f7a86ac1f3c73d261ef656e26d7fa86dafc94
gumingcn <gumingcn@gmail.com>
2023-06-20 13:41:18 +0800

    updated yarn start

--- shell/container_builder.sh
+++ shell/container_builder.sh
@@ -1,3 +1,3 @@
-# docker build --tag nodeapp:latest .
+docker build --tag nodeapp:latest .
 docker rm $(docker container ls -af name=nodeapp -q)
-docker run -it --name nodeapp -p 8000:8000 552598673490.dkr.ecr.ap-southeast-1.amazonaws.com/coca:e93aa18808a35ce5870a9e8f8419cd04d1cb5793
+docker run -it --name nodeapp -p 8000:8000 nodeapp
